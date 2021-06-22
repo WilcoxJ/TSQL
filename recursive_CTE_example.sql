0d98914dcc62379f1099a0602b94d858da9acd73
Josh Wilcox <wilcoxjosh505@gmail.com>
2021-06-22 17:16:49 -0600

    Update recursive_CTE_example.sql

--- recursive_CTE_example.sql
+++ recursive_CTE_example.sql
@@ -1,7 +1,6 @@
--- Recursive CTE's Explained:
+-- Recursive CTE:
 
--- In this example we have a table with data on several loans including the start date of the loan, loan term (how many months the loan will last), and monthly payment amount.
--- I will demonstrate how to create a recursive CTE that will return the SUM of payments made each month for the past year.
+-- example of recursive CTE that will return the SUM of payments made each month for the past year.
 
 -- Create Table
 CREATE TABLE [dbo].[loan](
@@ -47,26 +46,19 @@ SELECT monthly_payment, DATEADD(MONTH, loan_term, starting_date) as loan_end_dat
     -- The anchor member must pull from an exisitng table or view, and will be the base result set R[0] that is passed to the recursive member.
 UNION ALL
 -- Recursive Member 
-SELECT monthly_payment, loan_end_date, starting_date, DATEADD(MONTH, 1, current_month) -- Notice that we are incrementing the current_month each time the next result set R[1], R[2],...R[n] is passed to the recursive member. This is crucial to our terminating condition
+SELECT monthly_payment, loan_end_date, starting_date, DATEADD(MONTH, 1, current_month) -- incrementing the current_month each time the next result set R[1], R[2],...R[n] is passed to the recursive member. This is crucial to our terminating condition
     FROM loanCTE -- Notice the FROM loanCTE. This is the self-referential (hence recursive) part of the CTE.
-    WHERE current_month < loan_end_date -- Terminating condition. This tells the recursive member when to stop pulling in the result sets. Without this condition along with the incrementing date in the SELECT, we would have an infinite loop.
+    WHERE current_month < loan_end_date -- Terminating condition. This tells the recursive member when to stop pulling in the result sets.
 )
 
--- This example uses a DATE column as it's terminating condition, but you can also use exact numeric types. Avoid using approximate numerics such as floats like the plague.
--- The important thing to understand here is the anchor member is your base result set R[0], which is passed to the recursive member for the next iteration.
--- Next the recursive member executes with the input result set from the previous iteration R[i-1] and returns a sub-result set R[i] until the terminating condition is met.
--- Then all result sets R[0], R[1], … R[n] are combined using UNION ALL operator to get the full loanCTE result set.
-
-
--- Now that we have the loanCTE built we can write our final query to produce the desired list (Year, Month, and SUM of monthly payments for the past year).
 SELECT YEAR(current_month) as Year, MONTH(current_month) as Month,  SUM(monthly_payment) as total_montly_payments
     FROM loanCTE
-    WHERE DATEFROMPARTS(YEAR(current_month), MONTH(current_month), 1) BETWEEN GETDATE()-365 AND GETDATE() -- Notice the DATEFROMPARTS() function here uses 1 as the day parameter. This is important since current_month will often not have 1 as it's day, and if you were only to use WHERE current_month BETWEEN GETDATE()-365 AND GETDATE() the SUM of monthly payments would be incorrect.
+    WHERE DATEFROMPARTS(YEAR(current_month), MONTH(current_month), 1) BETWEEN GETDATE()-365 AND GETDATE()
     GROUP BY YEAR(current_month), MONTH(current_month) 
     ORDER BY YEAR(current_month) DESC, MONTH(current_month) DESC;
 
 -- Another Important note is SQL server by default only allows 100 iterations. You can increase this by setting the OPTION (MAXRECURSION 5000) at the bottom of the CTE
--- You can set this as high as 32767, or 0 which will remove the limit altogether.
+-- You can set this as high as 32767, or 0 which will remove the limit altogether (but beware the infinite loop!)
 
 -- Link to documentation: https://docs.microsoft.com/en-us/sql/t-sql/queries/with-common-table-expression-transact-sql?view=sql-server-ver15
 
