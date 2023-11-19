b9f5b0358c5e94f0a081601c16b6eec73526520b
Josh Wilcox <wilcoxjosh505@gmail.com>
2023-11-18 22:26:23 -0700

    temporal

--- temporal_table.sql
+++ temporal_table.sql
@@ -1,45 +1,73 @@
+CREATE TABLE dbo.Users
+(
+    UserID INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Users PRIMARY KEY,
+    UserName NVARCHAR(50) NOT NULL
+);
+
+CREATE TABLE dbo.Projects
+(
+    ProjectID INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_Projects PRIMARY KEY,
+    ProjectName NVARCHAR(50) NOT NULL
+);
+
 CREATE TABLE dbo.UserAssignment
 (
-    AssignmentID INT IDENTITY NOT NULL,
-    UserID INT NOT NULL,
-    ProjectID INT NOT NULL,
+    AssignmentID INT IDENTITY(1,1) NOT NULL CONSTRAINT PK_UserAssignment PRIMARY KEY,
+    UserID INT NOT NULL CONSTRAINT FK_UserAssignment_Users_UserID FOREIGN KEY REFERENCES dbo.Users(UserID),
+    ProjectID INT NOT NULL CONSTRAINT FK_UserAssignment_Projects_ProjectID FOREIGN KEY REFERENCES dbo.Projects(ProjectID),
     Amount DECIMAL(18,2) NOT NULL,
-    ValidFromUTC DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL,
-    ValidToUTC DATETIME2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL,
+    ValidFromUTC DATETIME2 GENERATED ALWAYS AS ROW START NOT NULL,
+    ValidToUTC DATETIME2 GENERATED ALWAYS AS ROW END NOT NULL,
     PERIOD FOR SYSTEM_TIME (ValidFromUTC, ValidToUTC)
 );
 
--- Enable system versioning on the table
 ALTER TABLE dbo.UserAssignment
     SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.UserAssignmentHistory));
 
--- Insert some initial data
+INSERT INTO dbo.Users (UserName)
+VALUES ('Wyatt Earp'),
+       ('Doc Holliday'),
+       ('Ike Clanton'),
+       ('Virgil Earp');
+
+INSERT INTO dbo.Projects (ProjectName)
+VALUES ('Project A'),
+       ('Project B'),
+       ('Project C');
+
 INSERT INTO dbo.UserAssignment (UserID, ProjectID, Amount)
 VALUES 
-    (101, 1, 5000.00),
-    (102, 2, 7500.00),
-    (103, 2, 6500.00),
-    (104, 1, 5500.00);
+    (1, 1, 5000.00),
+    (2, 2, 7500.00),
+    (3, 2, 6500.00),
+    (4, 1, 5500.00);
 
--- Update an assignment amount
 UPDATE dbo.UserAssignment
 SET Amount = 6000.00
-WHERE UserID = 101 AND ProjectID = 1;
+WHERE UserID = 1 AND ProjectID = 1;
+
+UPDATE dbo.UserAssignment
+SET Amount = 9000.00
+WHERE UserID = 2 AND ProjectID = 2;
 
 UPDATE dbo.UserAssignment
 SET Amount = 6800.00
-WHERE UserID = 103 AND ProjectID = 2;
+WHERE UserID = 3 AND ProjectID = 2;
 
 INSERT INTO dbo.UserAssignment (UserID, ProjectID, Amount)
 VALUES 
-    (105, 3, 8000.00),
+    (1, 3, 8000.00);
 
--- Query the current state of the table
-SELECT AssignmentID, UserID, ProjectID, Amount, ValidFromUTC, ValidToUTC
-FROM dbo.UserAssignment;
+-- view the entire history of table
+SELECT ua.AssignmentID, u.UserName, p.ProjectName, ua.Amount, ua.ValidFromUTC, ua.ValidToUTC
+FROM dbo.UserAssignment FOR SYSTEM_TIME ALL ua
+INNER JOIN dbo.Users u ON ua.UserID = u.UserID
+INNER JOIN dbo.Projects p ON ua.ProjectID = p.ProjectID
+ORDER BY AssignmentID, ValidFromUTC ASC;
 
--- Query the history of changes for all records
-SELECT AssignmentID, UserID, ProjectID, Amount, ValidFromUTC, ValidToUTC
-FROM dbo.UserAssignment
-FOR SYSTEM_TIME ALL;
--- other options: FOR SYSTEM_TIME AS OF '2023-01-15', SYSTEM_TIME BETWEEN '2023-01-01' AND '2023-02-01'
+-- view it for a set point in time 
+SELECT ua.AssignmentID, u.UserName, p.ProjectName, ua.Amount, ua.ValidFromUTC, ua.ValidToUTC
+FROM dbo.UserAssignment FOR SYSTEM_TIME AS OF '2023-01-19 05:20:11.2282792' ua -- other options include FOR SYSTEM_TIME BETWEEN '2023-01-01' AND '2023-02-01'
+INNER JOIN dbo.Users u ON ua.UserID = u.UserID
+INNER JOIN dbo.Projects p ON ua.ProjectID = p.ProjectID
+ORDER BY AssignmentID, ValidFromUTC ASC;
