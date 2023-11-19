5f096909c34833fe2f29379d916639443a8e4eee
Josh Wilcox <wilcoxjosh505@gmail.com>
2023-11-19 13:21:59 -0700

    recursive_cte

--- ammortization_schedule.sql
+++ ammortization_schedule.sql
@@ -1,6 +1,3 @@
--- Drop table if exists
-DROP TABLE IF EXISTS dbo.AmortizationSchedule;
-
 CREATE TABLE dbo.AmortizationSchedule (
     PaymentNumber INT,
     PaymentDate DATE,
@@ -11,14 +8,16 @@ CREATE TABLE dbo.AmortizationSchedule (
     EndingBalance DECIMAL(18, 2)
 );
 
-DECLARE @LoanAmount DECIMAL(18, 2) = 250000.00; -- Loan amount
-DECLARE @AnnualInterestRate DECIMAL(5, 3) = 3.275; -- Annual interest rate
+DECLARE @LoanAmount DECIMAL(18, 2) = 250000; -- Loan amount
+DECLARE @AnnualInterestRate DECIMAL(5, 3) = 3.175; -- Annual interest rate
 DECLARE @LoanTermYears INT = 30; -- Loan term in years
+DECLARE @LoanStartDate DATETIME = '2022-07-24'; -- Start date of loan
 
 -- Calculate monthly interest rate and number of payments
 DECLARE @MonthlyInterestRate DECIMAL(18, 6) = @AnnualInterestRate / 1200.0;
 DECLARE @NumberOfPayments INT = @LoanTermYears * 12;
 DECLARE @MonthlyPayment DECIMAL(18, 2);
+
 
 -- Calculate monthly payment using the PMT function
 SET @MonthlyPayment = (@LoanAmount * @MonthlyInterestRate) / (1 - POWER(1 + @MonthlyInterestRate, -@NumberOfPayments));
@@ -26,12 +25,13 @@ SET @MonthlyPayment = (@LoanAmount * @MonthlyInterestRate) / (1 - POWER(1 + @Mon
 WITH AmortizationCTE AS (
     SELECT
         1 AS PaymentNumber,
-        DATEADD(MONTH, 1, GETDATE()) AS PaymentDate,
+        DATEADD(MONTH, 1, @LoanStartDate) AS PaymentDate,
         CAST(@LoanAmount AS DECIMAL(18, 2)) AS BeginningBalance,
         @MonthlyPayment AS MonthlyPayment,
-        CAST(@LoanAmount * @MonthlyInterestRate AS DECIMAL(18, 2)) AS Interest,
+        CAST(ROUND(@LoanAmount * @MonthlyInterestRate,2) AS DECIMAL(18, 2)) AS Interest,
         CAST(@MonthlyPayment - (@LoanAmount * @MonthlyInterestRate) AS DECIMAL(18, 2)) AS Principal,
-        CAST(@LoanAmount - (@MonthlyPayment - (@LoanAmount * @MonthlyInterestRate)) AS DECIMAL(18, 2)) AS EndingBalance
+        CAST(@LoanAmount - (@MonthlyPayment - (@LoanAmount * @MonthlyInterestRate)) AS DECIMAL(18, 2)) AS EndingBalance,
+        CAST(@LoanAmount * @MonthlyInterestRate AS DECIMAL(18, 2)) AS CumulativeInterest
 
     UNION ALL
 
@@ -42,7 +42,8 @@ WITH AmortizationCTE AS (
         @MonthlyPayment,
         CAST(EndingBalance * @MonthlyInterestRate AS DECIMAL(18, 2)),
         CAST(@MonthlyPayment - (EndingBalance * @MonthlyInterestRate) AS DECIMAL(18, 2)),
-        CAST(EndingBalance - (@MonthlyPayment - (EndingBalance * @MonthlyInterestRate)) AS DECIMAL(18, 2))
+        CAST(EndingBalance - (@MonthlyPayment - (EndingBalance * @MonthlyInterestRate)) AS DECIMAL(18, 2)),
+        CAST(Interest + EndingBalance * @MonthlyInterestRate AS DECIMAL(18, 2))
     FROM
         AmortizationCTE
     WHERE
